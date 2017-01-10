7fc94e419f6082cb391a9fb1f88fd70a0875e4e2
Laura Abbott <labbott@redhat.com>
2017-01-10 13:35:49 -0800

    BACKPORT: arm64: Use __pa_symbol for kernel symbols (partial)
    (cherry-pick from commit 2077be6783b5936c3daa838d8addbb635667927f)
    
    __pa_symbol is technically the marcro that should be used for kernel
    symbols. Switch to this as a pre-requisite for DEBUG_VIRTUAL which
    will do bounds checking.
    
    Reviewed-by: Mark Rutland <mark.rutland@arm.com>
    Tested-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Laura Abbott <labbott@redhat.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Bug: 20045882
    Bug: 63737556
    Change-Id: Ibef89e5935c9562fa69e946778c705636c1ca61e

--- arch/arm64/kernel/vdso.c
+++ arch/arm64/kernel/vdso.c
@@ -122,6 +122,7 @@ static int __init vdso_init(void)
 {
 	int i;
 	struct page **vdso_pagelist;
+	unsigned long pfn;
 
 	if (memcmp(&vdso_start, "\177ELF", 4)) {
 		pr_err("vDSO is not a valid ELF object!\n");
