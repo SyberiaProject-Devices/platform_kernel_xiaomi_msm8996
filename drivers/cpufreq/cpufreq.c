d411048759eb2a359d39b625b5d361db37cf61ab
DennySPB <dennyspb@gmail.com>
2018-05-28 11:57:14 +0300

    gemini: cpufreq: limit little cluster to 1478 mhz
    Schedutil prefers to set max freq during interraction boost.
    According to litle cluster power curve, 1593 mhz is most energy consumption frequency (+40 mv on this step).
    
    https://forum.xda-developers.com/oneplus-3/how-to/advanced-interactive-governor-tweaks-t3476589),
    
    We can limit max cpu freq for little cluster to 1478 mhz without significant performance regression,
    but it can help for energy saving.
    
    Also we can disable limitation in runtime via writing 'Y' in /sys/module/parameters/disable_underclock
    
    inspired in: https://review.lineageos.org/#/c/LineageOS/android_device_xiaomi_msm8998-common/+/215342/

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2360,6 +2360,13 @@ int cpufreq_get_policy(struct cpufreq_policy *policy, unsigned int cpu)
 }
 EXPORT_SYMBOL(cpufreq_get_policy);
 
+#ifdef CONFIG_MACH_XIAOMI_A1
+#define UNDERCLK_MAX_LITTLECL 1478400
+static bool disable_underclock;
+module_param_named(disable_underclock,
+    disable_underclock, bool, S_IRUGO | S_IWUSR | S_IWGRP);
+#endif
+
 /*
  * policy : current policy.
  * new_policy: policy to be set.
@@ -2369,6 +2376,15 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 {
 	struct cpufreq_governor *old_gov;
 	int ret;
+
+#ifdef CONFIG_MACH_XIAOMI_A1
+    if (!disable_underclock) {
+	if (new_policy->cpu < 2) {
+	    if (new_policy->max > UNDERCLK_MAX_LITTLECL)
+		new_policy->max = UNDERCLK_MAX_LITTLECL;
+	}
+    }
+#endif
 
 	pr_debug("setting new policy for CPU %u: %u - %u kHz\n",
 		 new_policy->cpu, new_policy->min, new_policy->max);
