7d06844e403192fd372e5a902121aa272457ca14
Jan Engelmohr <jan.engelmohr@mailbox.tu-dresden.de>
2018-06-24 16:12:50 +0200

    Codecs: Fix conversation from HZ to msecs_to_jiffies
    Originally (with HZ=100) there are 10 jiffies each tick, so if we use msecs_to_jiffies the input needs to be 10 times bigger.
    
    Change-Id: Ib281b0fc3b62d3ab8535ff1d637b38c7c465c0ef

--- sound/soc/codecs/tfa98xx.c
+++ sound/soc/codecs/tfa98xx.c
@@ -744,7 +744,7 @@ static ssize_t tfa98xx_dbgfs_dsp_state_set(struct file *file,
 	} else if (!strncmp(buf, mon_start_cmd, sizeof(mon_start_cmd) - 1)) {
 		pr_info("Manual start of monitor thread...\n");
 		queue_delayed_work(tfa98xx->tfa98xx_wq,
-				&tfa98xx->monitor_work, HZ);
+					&tfa98xx->monitor_work, msecs_to_jiffies(1000));
 	} else if (!strncmp(buf, mon_stop_cmd, sizeof(mon_stop_cmd) - 1)) {
 		pr_info("Manual stop of monitor thread...\n");
 		cancel_delayed_work_sync(&tfa98xx->monitor_work);
@@ -1816,7 +1816,7 @@ static void tfa98xx_tapdet_check_update(struct tfa98xx *tfa98xx)
 		tfa98xx->tapdet_poll = true;
 		if (enable)
 			queue_delayed_work(tfa98xx->tfa98xx_wq,
-				&tfa98xx->tapdet_work, HZ/10);
+						&tfa98xx->tapdet_work, msecs_to_jiffies(100));
 		else
 			cancel_delayed_work_sync(&tfa98xx->tapdet_work);
 		dev_dbg(tfa98xx->codec->dev,
@@ -2145,7 +2145,7 @@ static void tfa98xx_tapdet_work(struct work_struct *work)
 	if (val & TFA98XX_STATUSREG_SPKS)
 		tfa98xx_tapdet(tfa98xx);
 
-	queue_delayed_work(tfa98xx->tfa98xx_wq, &tfa98xx->tapdet_work, HZ/10);
+	queue_delayed_work(tfa98xx->tfa98xx_wq, &tfa98xx->tapdet_work, msecs_to_jiffies(100));
 }
 
 static void tfa98xx_monitor(struct work_struct *work)
@@ -2199,7 +2199,7 @@ static void tfa98xx_monitor(struct work_struct *work)
 	}
 
 	/* reschedule */
-	queue_delayed_work(tfa98xx->tfa98xx_wq, &tfa98xx->monitor_work, 5 * HZ);
+	queue_delayed_work(tfa98xx->tfa98xx_wq, &tfa98xx->monitor_work, msecs_to_jiffies(5000));
 }
 
 static void tfa98xx_dsp_init(struct tfa98xx *tfa98xx)
@@ -2247,8 +2247,8 @@ static void tfa98xx_dsp_init(struct tfa98xx *tfa98xx)
 			 * needed.
 			 */
 			queue_delayed_work(tfa98xx->tfa98xx_wq,
-					&tfa98xx->monitor_work,
-					1 * HZ);
+						&tfa98xx->monitor_work,
+						msecs_to_jiffies(1000));
 		}
 	} else {
 		/* exceeded max number ot start tentatives, cancel start */
