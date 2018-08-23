b23f8514eafe038b5214fd7d7928123609eecd96
DennySPB <dennyspb@gmail.com>
2018-08-23 11:01:48 +0300

    [SQUASH] Update binder to P from Benzo
    binder: use standard functions to allocate fds
    binder: clarify deferred thread work
    binder: no outgoing transaction when thread todo has transaction
    binder: Rate-limit debug and userspace triggered err msg
    binder: Show extra_buffers_size in trace
    binder: support dedicated node threads
    binder: Add binder_get_thread_by_pid()
    binder: Use ERR_PTR for binder_new_node()
    binder: change down_write to down_read
    binder: correct the cmd print for BINDER_WORK_RETURN_ERROR
    binder: remove 32-bit binder interface
    binder: Use true and false for boolean values
    binder: Use octal permissions
    binder: Prefer __func__ to using hardcoded function name
    binder: Check for errors in binder_alloc_shrinker_init()
    binder: prevent transactions into own process
    binder: check for binder_thread allocation failure in binder_poll()
    binder: synchronize_rcu() when using POLLFREE
    binder: remove WARN() for redundant txn error
    binder: remove waitqueue when thread exits
    binder: Remove obsolete proc waitqueue
    binder: move to the "real" part of the kernel

--- drivers/android/binder_alloc.h
+++ drivers/android/binder_alloc.h
@@ -44,22 +44,22 @@ struct binder_transaction;
  * Bookkeeping structure for binder transaction buffers
  */
 struct binder_buffer {
-	struct list_head entry; /* free and allocated entries by address */
-	struct rb_node rb_node; /* free entry by size or allocated entry */
-				/* by address */
-	unsigned free:1;
-	unsigned allow_user_free:1;
-	unsigned async_transaction:1;
-	unsigned free_in_progress:1;
-	unsigned debug_id:28;
+    struct list_head entry; /* free and allocated entries by address */
+    struct rb_node rb_node; /* free entry by size or allocated entry */
+		/* by address */
+    unsigned free:1;
+    unsigned allow_user_free:1;
+    unsigned async_transaction:1;
+    unsigned free_in_progress:1;
+    unsigned debug_id:28;
 
-	struct binder_transaction *transaction;
+    struct binder_transaction *transaction;
 
-	struct binder_node *target_node;
-	size_t data_size;
-	size_t offsets_size;
-	size_t extra_buffers_size;
-	void *data;
+    struct binder_node *target_node;
+    size_t data_size;
+    size_t offsets_size;
+    size_t extra_buffers_size;
+    void *data;
 };
 
 /**
@@ -69,9 +69,9 @@ struct binder_buffer {
  * @alloc:    binder_alloc for a proc
  */
 struct binder_lru_page {
-	struct list_head lru;
-	struct page *page_ptr;
-	struct binder_alloc *alloc;
+    struct list_head lru;
+    struct page *page_ptr;
+    struct binder_alloc *alloc;
 };
 
 /**
@@ -100,20 +100,20 @@ struct binder_lru_page {
  * struct binder_buffer objects used to track the user buffers
  */
 struct binder_alloc {
-	struct mutex mutex;
-	struct vm_area_struct *vma;
-	struct mm_struct *vma_vm_mm;
-	void *buffer;
-	ptrdiff_t user_buffer_offset;
-	struct list_head buffers;
-	struct rb_root free_buffers;
-	struct rb_root allocated_buffers;
-	size_t free_async_space;
-	struct binder_lru_page *pages;
-	size_t buffer_size;
-	uint32_t buffer_free;
-	int pid;
-	size_t pages_high;
+    struct mutex mutex;
+    struct vm_area_struct *vma;
+    struct mm_struct *vma_vm_mm;
+    void *buffer;
+    ptrdiff_t user_buffer_offset;
+    struct list_head buffers;
+    struct rb_root free_buffers;
+    struct rb_root allocated_buffers;
+    size_t free_async_space;
+    struct binder_lru_page *pages;
+    size_t buffer_size;
+    uint32_t buffer_free;
+    int pid;
+    size_t pages_high;
 };
 
 #ifdef CONFIG_ANDROID_BINDER_IPC_SELFTEST
@@ -122,28 +122,28 @@ void binder_selftest_alloc(struct binder_alloc *alloc);
 static inline void binder_selftest_alloc(struct binder_alloc *alloc) {}
 #endif
 enum lru_status binder_alloc_free_page(struct list_head *item,
-				       spinlock_t *lock, void *cb_arg);
+		       spinlock_t *lock, void *cb_arg);
 extern struct binder_buffer *binder_alloc_new_buf(struct binder_alloc *alloc,
-						  size_t data_size,
-						  size_t offsets_size,
-						  size_t extra_buffers_size,
-						  int is_async);
+			  size_t data_size,
+			  size_t offsets_size,
+			  size_t extra_buffers_size,
+			  int is_async);
 extern void binder_alloc_init(struct binder_alloc *alloc);
-void binder_alloc_shrinker_init(void);
+extern int binder_alloc_shrinker_init(void);
 extern void binder_alloc_vma_close(struct binder_alloc *alloc);
 extern struct binder_buffer *
 binder_alloc_prepare_to_free(struct binder_alloc *alloc,
-			     uintptr_t user_ptr);
+	         uintptr_t user_ptr);
 extern void binder_alloc_free_buf(struct binder_alloc *alloc,
-				  struct binder_buffer *buffer);
+		  struct binder_buffer *buffer);
 extern int binder_alloc_mmap_handler(struct binder_alloc *alloc,
-				     struct vm_area_struct *vma);
+		     struct vm_area_struct *vma);
 extern void binder_alloc_deferred_release(struct binder_alloc *alloc);
 extern int binder_alloc_get_allocated_count(struct binder_alloc *alloc);
 extern void binder_alloc_print_allocated(struct seq_file *m,
-					 struct binder_alloc *alloc);
+		     struct binder_alloc *alloc);
 void binder_alloc_print_pages(struct seq_file *m,
-			      struct binder_alloc *alloc);
+	          struct binder_alloc *alloc);
 
 /**
  * binder_alloc_get_free_async_space() - get free space available for async
@@ -154,12 +154,12 @@ void binder_alloc_print_pages(struct seq_file *m,
 static inline size_t
 binder_alloc_get_free_async_space(struct binder_alloc *alloc)
 {
-	size_t free_async_space;
+    size_t free_async_space;
 
-	mutex_lock(&alloc->mutex);
-	free_async_space = alloc->free_async_space;
-	mutex_unlock(&alloc->mutex);
-	return free_async_space;
+    mutex_lock(&alloc->mutex);
+    free_async_space = alloc->free_async_space;
+    mutex_unlock(&alloc->mutex);
+    return free_async_space;
 }
 
 /**
@@ -172,16 +172,15 @@ binder_alloc_get_free_async_space(struct binder_alloc *alloc)
 static inline ptrdiff_t
 binder_alloc_get_user_buffer_offset(struct binder_alloc *alloc)
 {
-	/*
-	 * user_buffer_offset is constant if vma is set and
-	 * undefined if vma is not set. It is possible to
-	 * get here with !alloc->vma if the target process
-	 * is dying while a transaction is being initiated.
-	 * Returning the old value is ok in this case and
-	 * the transaction will fail.
-	 */
-	return alloc->user_buffer_offset;
+    /*
+     * user_buffer_offset is constant if vma is set and
+     * undefined if vma is not set. It is possible to
+     * get here with !alloc->vma if the target process
+     * is dying while a transaction is being initiated.
+     * Returning the old value is ok in this case and
+     * the transaction will fail.
+     */
+    return alloc->user_buffer_offset;
 }
 
 #endif /* _LINUX_BINDER_ALLOC_H */
-
